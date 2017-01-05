dc502fd8e7d3b98e786887e1fe4f7568dc75f43b
Ilya Rostovtsev <ilyaforpresident@gmail.com>
2017-01-04 23:17:30 -0800

    templated Vector3 and Vector2 seem like a good way to begin

---
+++ pbrt3/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  pbrt3
+//
+//  Created by Ilya Rostovtsev on 1/3/17.
+//  Copyright © 2017 Ilya Rostovtsev. All rights reserved.
+//
+
+#include <iostream>
+#include "geometry.h"
+
+template <typename T>
+void pv( const Vector2< T >& v )
+{
+    std::cout << v.x << ", " << v.y << '\n';
+}
+
+int main(int argc, const char * argv[])
+{    
+    return 0;
+}
